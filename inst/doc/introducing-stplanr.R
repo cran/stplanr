98fa6ed0a8699cf6915dde65e55bdfde4f978aa6
Robin Lovelace <rob00x@gmail.com>
2018-06-02 21:56:47 +0000

    version 0.2.5

--- inst/doc/introducing-stplanr.R
+++ inst/doc/introducing-stplanr.R
@@ -14,6 +14,9 @@ knitr::opts_chunk$set(eval = build)
 library(stplanr)
 
 ## ------------------------------------------------------------------------
+data(package = "stplanr")$result[, "Item"]
+
+## ------------------------------------------------------------------------
 od_eg = read.csv(text = 
   "origin, destination, V1, V2
   1, 2, 100, 3
@@ -22,27 +25,26 @@ od_eg = read.csv(text =
 knitr::kable(od_eg)
 
 ## ------------------------------------------------------------------------
-data("flow") # load the 'flow' dataset from the stplanr package
 head(flow[c(1:3, 12)])
 
 ## ------------------------------------------------------------------------
-data("cents") # load the 'cents' dataset
-head(cents)
+head(cents_sf)
 
 ## ------------------------------------------------------------------------
-library(tmap)
-tmap_mode("view")
-qtm(cents, symbols.size = 5)
+library(sf)
+plot(st_geometry(cents_sf))
 
 ## ------------------------------------------------------------------------
 flow_single_line = flow[4,] # select only the first line
 desire_line_single = od2line(flow = flow_single_line, zones = cents)
 
 ## ------------------------------------------------------------------------
-qtm(desire_line_single, lines.lwd = 5)
+desire_line_single = st_as_sf(desire_line_single)
+plot(st_geometry(desire_line_single), lwd = 5)
+plot(cents_sf, add = TRUE, cex = 5)
 
 ## ------------------------------------------------------------------------
-l = od2line(flow = flow, zones = cents)
+l = od2line(flow = flow, zones = cents_sf)
 # identify 'intrazone flows'
 sel_intra = l$Area.of.residence == l$Area.of.workplace
 # find distances
@@ -53,13 +55,14 @@ sel = !sel_intra & sel_dist
 l = l[sel, ]
 
 ## ---- eval=FALSE---------------------------------------------------------
-#  qtm(l)
+#  plot(l)
 
 ## ------------------------------------------------------------------------
-qtm(l, lines.lwd = "All", scale = 10)
+lwd = l$All / mean(l$All)
+plot(st_geometry(l), lwd = lwd)
 
 ## ------------------------------------------------------------------------
-tm_shape(l) + tm_lines(lwd = "All", scale = 10, col = "Bicycle")
+plot(l["Bicycle"], lwd = lwd)
 
 ## ---- message=FALSE, warning=FALSE---------------------------------------
 # if the next line returns FALSE the code will not run
@@ -71,7 +74,18 @@ if(has_internet & cs_key == 32) {
   r = routes_fast[sel, ]
 }
 
-## ------------------------------------------------------------------------
-r@data = cbind(r@data, l@data)
-tm_shape(r) + tm_lines(lwd = "All", scale = 10, col = "Bicycle")
+## ---- out.width="500", out.height="500"----------------------------------
+r_sf = st_sf(l, geometry = st_as_sfc(r))
+plot(r_sf["Bicycle"], lwd = lwd * 3, reset = FALSE)
+
+## ---- out.width="500", out.height="500", echo=FALSE, eval=FALSE----------
+#  # alternative showing buildings:
+#  r_sf = st_sf(l, geometry = st_as_sfc(r))
+#  if(require(osmdata)) {
+#    buildings = opq(st_bbox(l)) %>%
+#    add_osm_feature(key = "building", value = "industrial") %>%
+#    osmdata_sf()
+#  }
+#  plot(r_sf["Bicycle"], lwd = lwd * 3, reset = FALSE)
+#  plot(st_geometry(buildings$osm_polygons), col = "grey", add = TRUE)
 
